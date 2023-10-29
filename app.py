2a6098f21d74e7c4a851a14e80e5855f965646fb
Piyush Jeena <56345075+pjeena@users.noreply.github.com>
2023-10-29 16:20:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 
 
-api_key = 'AIzaSyAolOXNAoKHZWOlfd-Qn4pbbW3WZepJ6wU' # get this free api key from https://makersuite.google.com/
+api_key = '' # get this free api key from https://makersuite.google.com/
 
 llm = GooglePalm(google_api_key=api_key, temperature=0.1)
 
